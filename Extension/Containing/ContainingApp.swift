697756ce56accaefa8e1dae741816a2638dccd01
Marc Stibane <marc@taler.net>
2024-07-12 18:35:45 +0200

    Containing

---
+++ Extension/Containing/ContainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContainingApp.swift
+//  Containing
+//
+//  Created by Marc Stibane on 2024-07-03.
+//
+
+import SwiftUI
+
+@main
+struct ContainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Extension/Containing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Containing
+//
+//  Created by Marc Stibane on 2024-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, ContainingApp!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
