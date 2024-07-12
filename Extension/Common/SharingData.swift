85a7b08c0d833484cdd7700ded6a312b41f46984
Marc Stibane <marc@taler.net>
2024-07-12 18:37:18 +0200

    Common

---
+++ Extension/Common/SharingData.swift
@@ -0,0 +1,61 @@
+//
+//  SharingData.swift
+//  Common
+//
+//  Created by Marc Stibane on 2024-07-04.
+//
+import Foundation
+import SwiftUI
+
+enum SharingError: Error {
+    case userCanceled
+    case inputItemError
+    case inputItemAttachmentError
+}
+
+// MARK: -
+class ItemSource: NSObject, UIActivityItemSource {
+    let dataToShare: Any
+
+    init(dataToShare: Any) {
+        self.dataToShare = dataToShare
+    }
+
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return dataToShare
+    }
+
+    func activityViewController(_ activityViewController: UIActivityViewController,
+                        itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        if let activityType, activityType == .copyToPasteboard {
+            return "copyToPasteboard"
+        } else {
+            return dataToShare
+        }
+    }
+}
+// MARK: -
+struct ShareInput: Codable {
+    var inputStr: String
+    // ... more input data
+}
+
+extension ShareInput: Transferable {
+    static var transferRepresentation: some TransferRepresentation {
+        CodableRepresentation(contentType: .shareInput)
+//        ProxyRepresentation(exporting: \.inputStr)
+    }
+}
+
+// MARK: -
+struct ShareResult: Codable {
+    var outputStr: String
+    // ... more result data
+}
+
+extension ShareResult: Transferable {
+    static var transferRepresentation: some TransferRepresentation {
+        CodableRepresentation(contentType: .shareResult)
+//        ProxyRepresentation(exporting: \.outputStr)
+    }
+}
